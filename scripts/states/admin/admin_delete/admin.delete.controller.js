0f9279bc2f12e9ef658f3c0b6d9794889c6cd39f
Athul Philip <athulphilip1309@gmail.com>
2019-07-26 14:53:17 +0100

    Refactored select all logic
    Select all button and the list of checkboxes had a bug where it took a few clicks for it to work and then it was fine.
    
    This has been partially fixed. Now it does what it is supposed to for the most part and the only problem is a visual glitch which may be indicative of a fault within the logic.
    
    Need to comeback to this after implementing search bar directive.

--- scripts/states/admin/admin_delete/admin.delete.controller.js
+++ scripts/states/admin/admin_delete/admin.delete.controller.js
@@ -30,45 +30,36 @@
     var vm = angular.extend(this, {
       title: capitalize($state.params.entity),
       searchStr: "",
+      paginationSize: 10,
       entities: registeredEntities,
       selectedEntities: [],
+      // exceedsPaginationSize: vm.entities.length > vm.paginationSize,
       showName: $state.params.entity === 'species',
-      showPostcode: $state.params.entity === 'sightings'
+      showPostcode: $state.params.entity === 'sightings',
+      allSelected: false
     });
 
-    console.log(vm.entities);
-
     vm.toggleSelection = function toggleSelection(entity) {
-      var index = vm.selectedEntities.indexOf(entity);
 
+      let index = vm.selectedEntities.indexOf(entity);
       if (index > -1) {
         // Entity was already selected. Remove it from the array.
         vm.selectedEntities.splice(index, 1);
-        console.debug(`${vm.selectedEntities.length} entities currently selected.`);
-      }
-
-      // Not in the array of selected entities. Add it to the array.
-      else {
+      } else {
+        // Not in the array of selected entities. Add it to the array.
         vm.selectedEntities.push(entity);
-        console.debug(`${vm.selectedEntities.length} entities currently selected.`);
-        var checkboxes = document.querySelectorAll('input.admin-check'),
-          checkall = document.getElementById('admin-check-all');
-        checkboxes.indeterminate = true;
-        for (var i = 0; i < checkboxes.length; i++) {
-          checkboxes[i].onclick = function () {
-            var checkedCount = document.querySelectorAll('input.admin-check:checked').length;
-
-            checkall.checked = checkedCount > 0;
-            checkall.indeterminate = checkedCount > 0 && checkedCount < checkboxes.length;
-          }
-        }
-
-        checkall.onclick = function () {
-          for (var i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = this.checked;
-          }
-        }
       }
+
+      // Update the select all checkbox
+      let checkall = document.getElementById('admin-check-all');
+      checkall.checked = vm.selectedEntities.length === vm.entities.length;
+      checkall.indeterminate = vm.selectedEntities.length > 0 && vm.selectedEntities.length < vm.entities.length;
+
+    }
+
+    vm.selectAll = function selectAll() {
+      // Using angular.copy as it creates a new object and assigns that to vm.selectedEntities whereas
+      vm.allSelected ? angular.copy(vm.entities, vm.selectedEntities) : angular.copy([], vm.selectedEntities);
     }
 
 
