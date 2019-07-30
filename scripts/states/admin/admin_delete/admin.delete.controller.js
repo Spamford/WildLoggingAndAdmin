80fc253e781dc33e96383e6a61d25c08c4b9a6c5
Athul Philip <athulphilip1309@gmail.com>
2019-07-30 17:54:47 +0100

    Added more detail to sightings list
    Sightings list now fetches dependent species names to provide more information to the user than just a single postcode for each sighting.
    
    Now it shows the species that was seen and the postcode it was seen at.
    
    The number of network requests are also kept to a minimum.
     - Species are only requested when new species (species that aren't in the list of sightings already) are referenced in the new pages of sightings that the user requests.

--- scripts/states/admin/admin_delete/admin.delete.controller.js
+++ scripts/states/admin/admin_delete/admin.delete.controller.js
@@ -29,7 +29,7 @@
       placeholderProperty: $state.params.entity === 'species' ? 'name' : 'postcode',
       searchStr: "",
       lastSearch: "",
-      pageSize: 1,
+      pageSize: 3,
       nextPage: 1,
       entities: [],
       selectedEntities: [],
@@ -37,7 +37,8 @@
       showPostcode: $state.params.entity === 'sightings',
       allSelected: false,
       noMoreEntitiesToGet: true,
-      showSpinner: false
+      showSpinner: false,
+      fetchedUniqueSpeciesIDs: []
     });
 
     vm.toggleSelection = function toggleSelection(entity) {
@@ -108,12 +109,14 @@
         return;
       } else {
         vm.nextPage = 1;
+        vm.fetchedUniqueSpeciesIDs = [];
       }
 
       function successCallback(response) {
+        vm.entities = response.data;
+        if (vm.showPostcode) displayMoreInfo();
         vm.lastSearch = entityFilter;
         vm.noMoreEntitiesToGet = false;
-        vm.entities = response.data;
         vm.nextPage++;
         vm.showSpinner = false;
       }
@@ -137,6 +140,7 @@
 
       function successCallback(response) {
         vm.entities.push(...response.data);
+        if (vm.showPostcode) displayMoreInfo();
         vm.nextPage++;
         vm.noMoreEntitiesToGet = response.data.length === 0;
         vm.showSpinner = false;
@@ -155,6 +159,86 @@
         vm.showSpinner = true;
       }
 
+    }
+
+    function displayMoreInfo() {
+
+      let uniqueSpeciesIDs = [...new Set(vm.entities.map(x => x.thing))];
+
+      let alreadyFetchedSpecies = vm.entities.map((x) => {
+        if (!x.hasOwnProperty("speciesName")) {
+          for (let i = 0; i < vm.entities.length; i++) {
+            if (vm.entities[i].hasOwnProperty("speciesName") && x.thing === vm.entities[i].thing) {
+              return {
+                id: vm.entities[i].thing,
+                name: vm.entities[i].speciesName
+              }
+            }
+          }
+        }
+      });
+
+      alreadyFetchedSpecies = alreadyFetchedSpecies.filter((x) => x !== undefined);
+
+      function successCallback(response) {
+        assignFetchedSpeciesNames(response);
+        assignDuplicateSpeciesNames(alreadyFetchedSpecies);
+        vm.fetchedUniqueSpeciesIDs = uniqueSpeciesIDs;
+      }
+
+      function failureCallback(error) {
+        console.error(error);
+      }
+
+      if (uniqueSpeciesIDs.length > vm.fetchedUniqueSpeciesIDs.length) {
+        // Among the sightings fetched from the server, at least one of them is of a species that hasn't been requested from the server yet.
+        getUniqueSpecies(uniqueSpeciesIDs.diff(vm.fetchedUniqueSpeciesIDs)).then(successCallback, failureCallback);
+      } else {
+        // How are the names shown when the new species fetched from the server have a thing property that has already been fetched.
+        assignDuplicateSpeciesNames(alreadyFetchedSpecies)
+      }
+
+    }
+
+    function assignDuplicateSpeciesNames(alreadyFetchedSpecies) {
+      for (let i = 0; i < vm.entities.length; i++) {
+        if (!vm.entities[i].hasOwnProperty("speciesName")) {
+          for (let j = 0; j < alreadyFetchedSpecies.length; j++) {
+            if (vm.entities[i].thing === alreadyFetchedSpecies[j].id) {
+              vm.entities[i].speciesName = alreadyFetchedSpecies[j].name;
+            }
+          }
+        }
+      }
+    }
+
+    /**
+     * Assigns the names of the species for each sighting
+     * @param {Object} res Response from the server
+     */
+    function assignFetchedSpeciesNames(res) {
+      for (let i = 0; i < vm.entities.length; i++) {
+        for (let dataObj in res) {
+          if (res.hasOwnProperty(dataObj)) {
+            if (vm.entities[i].thing === res[dataObj].data.id) {
+              vm.entities[i].speciesName = res[dataObj].data.name;
+            }
+          }
+        }
+      }
+    }
+
+    // https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
+    Array.prototype.diff = function (a) {
+      return this.filter(i => a.indexOf(i) < 0)
+    }
+
+    function getUniqueSpecies(uniqueSpeciesIDArr) {
+      let promiseObj = $q.defer();
+      let promiseArray = uniqueSpeciesIDArr.map(speciesID => speciesSrvc.getSpeciesFromId(speciesID));
+      promiseObj.resolve($q.all(promiseArray));
+      promiseObj.reject(new Error("Failed to GET associated unique species for sightings"));
+      return promiseObj.promise;
     }
 
     return vm;
