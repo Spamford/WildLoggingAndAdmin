8104131bfb5866237decb61a128f5f1ee5ac4ee7
Athul Philip <athulphilip1309@gmail.com>
2019-07-31 17:16:52 +0100

    Refactored and commented admin delete controller
    Refactored duplicate code when getting entities into it's own function.
    
    Added comments for clarity

--- scripts/states/admin/admin_delete/admin.delete.controller.js
+++ scripts/states/admin/admin_delete/admin.delete.controller.js
@@ -20,25 +20,31 @@
         sightingsSrvc
     ) {
 
-        function capitalize(str) {
-            return str.charAt(0).toUpperCase() + str.slice(1)
-        }
-
-        var vm = angular.extend(this, {
+        let vm = angular.extend(this, {
             title: capitalize($state.params.entity),
-            placeholderProperty: $state.params.entity === 'species' ? 'name' : 'postcode',
+
+            // Search bar properties
             searchStr: "",
             lastSearch: "",
+            placeholderProperty: $state.params.entity === 'species' ? 'name' : 'postcode',
+
+            // Pagination request properties
             pageSize: 3,
             nextPage: 1,
+
+            // Array declarations
             entities: [],
             selectedEntities: [],
+            fetchedSpeciesIDs: [],
+
+            // Used to try interpolating the correct properties from the requested entites
             showName: $state.params.entity === 'species',
             showPostcode: $state.params.entity === 'sightings',
+
+            // Used to control behaviour of UI
             allSelected: false,
             noMoreEntitiesToGet: true,
-            searching: false,
-            fetchedUniqueSpeciesIDs: []
+            searching: false
         });
 
         vm.toggleSelection = function toggleSelection(entity) {
@@ -64,19 +70,45 @@
 
 
         vm.deleteSelectedEntities = function deleteSelectedEntities() {
-            vm.searching = true;
+            vm.searching = true; // Show spinner
             deleteEntities().then(
-                function success(response) {
+                function success() {
                     vm.searching = false;
+                    // Remove selected entities from list
                     angular.copy(vm.entities.diff(vm.selectedEntities), vm.entities);
+                    // Clear selected entities
                     angular.copy([], vm.selectedEntities);
-                    return response;
                 },
                 function failure(error) {
                     vm.searching = false;
-                    console.log("Oh no something went wrong... :) -> ", error);
+                    console.log(error);
                 }
             );
+        }
+
+        vm.searchForEntities = function searchForEntities(searchStr = "") { // searchStr defaults to "" to avoid undefined.
+            let lowercaseSearchStr = searchStr.toLowerCase();
+
+            // Returning as the "more" button is supposed to be used to get more pages of the same search.
+            if (vm.lastSearch === lowercaseSearchStr || lowercaseSearchStr === "") {
+                return;
+            } else {
+                // Searching for something different so emptying relevant arrays and setting relevant variables.
+                vm.noMoreEntitiesToGet = false;
+                vm.nextPage = 1;
+                vm.fetchedSpeciesIDs = [];
+                vm.selectedEntities = [];
+            }
+
+            getEntities(lowercaseSearchStr, true)
+        }
+
+        vm.getMoreEntities = function getMoreEntities() {
+            getEntities(vm.lastSearch, false)
+        }
+
+        function capitalize(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
         }
 
         function deleteEntities() {
@@ -99,23 +131,25 @@
             return promiseObj.promise;
         }
 
-        vm.getEntities = function getEntities(searchStr = "") {
+        /**
+         * Iterates over each element of the array that the method was called on, checks to see if that element is in the array that
+         * was passed in as an argument and keeps the elements that were not in the array that was passed thereby filtering out those elements.
+         * See https://stackoverflow.com/a/4026828 for more information
+         * @param {Array} arr Array containing elements to filter out. 
+         */
+        Array.prototype.diff = function diff(arr) {
+            // Here, this refers to the array that diff was called on.
+            return this.filter(hostArrElem => arr.indexOf(hostArrElem) < 0)
+        }
 
-            let entityFilter = searchStr.toLowerCase();
-
-            if (vm.lastSearch === entityFilter || entityFilter === "") {
-                vm.noMoreEntitiesToGet = false;
-                return;
-            } else {
-                vm.nextPage = 1;
-                vm.fetchedUniqueSpeciesIDs = [];
-            }
-
+        function getEntities(searchStr, saveSearch = false) {
             function successCallback(response) {
-                vm.entities = response.data;
+                // Need to replace vm.entities when saving search as it is a search for a different entity altogether.
+                // Otherwise the search results would get appended to the results from the previous search.
+                saveSearch ? vm.entities = response.data : vm.entities.push(...response.data);
                 if (vm.showPostcode) displayMoreInfo();
-                vm.lastSearch = entityFilter;
-                vm.noMoreEntitiesToGet = false;
+                vm.lastSearch = saveSearch ? searchStr : vm.lastSearch;
+                vm.noMoreEntitiesToGet = response.data.length === 0;
                 vm.nextPage++;
                 vm.searching = false;
             }
@@ -126,49 +160,27 @@
             }
 
             if (vm.showName) {
-                vm.searching = true;
-                speciesSrvc.getSpeciesByName(entityFilter, vm.pageSize, vm.nextPage).then(successCallback, failureCallback);
-            } else if (vm.showPostcode) {
-                vm.searching = true;
-                sightingsSrvc.getSightingsByName(entityFilter, vm.pageSize, vm.nextPage).then(successCallback, failureCallback);
-            }
-
-        }
-
-        vm.getMoreEntities = function getMoreEntities() {
-
-            function successCallback(response) {
-                vm.entities.push(...response.data);
-                if (vm.showPostcode) displayMoreInfo();
-                vm.nextPage++;
-                vm.noMoreEntitiesToGet = response.data.length === 0;
-                vm.searching = false;
-            }
-
-            function failureCallback(error) {
-                console.log("Failed to get MORE entities...", error);
-                vm.searching = false;
-            }
-
-            if (vm.showName) {
-                speciesSrvc.getSpeciesByName(vm.lastSearch, vm.pageSize, vm.nextPage).then(successCallback, failureCallback)
+                speciesSrvc.getSpeciesByName(searchStr, vm.pageSize, vm.nextPage).then(successCallback, failureCallback);
                 vm.searching = true;
             } else if (vm.showPostcode) {
-                sightingsSrvc.getSightingsByName(vm.lastSearch, vm.pageSize, vm.nextPage).then(successCallback, failureCallback);
+                sightingsSrvc.getSightingsByName(searchStr, vm.pageSize, vm.nextPage).then(successCallback, failureCallback);
                 vm.searching = true;
             }
-
         }
 
         function displayMoreInfo() {
 
-            // https://gist.github.com/telekosmos/3b62a31a5c43f40849bb#gistcomment-1830283
-            let uniqueSpeciesIDs = [...new Set(vm.entities.map(x => x.thing))];
+            if (vm.showName) return; // Only do this for admin delete sightings page
 
-            let alreadyFetchedSpecies = vm.entities.map((x) => {
-                if (!x.hasOwnProperty("speciesName")) {
+            // https://gist.github.com/telekosmos/3b62a31a5c43f40849bb#gistcomment-1830283
+            // Filter out duplicates and create array of the new species IDs from requesting another page.
+            let newSpeciesIDs = [...new Set(vm.entities.map(entity => entity.thing))];
+
+            // Find duplicates
+            let alreadyFetchedSpecies = vm.entities.map(entity => {
+                if (!entity.hasOwnProperty("speciesName")) {
                     for (let i = 0; i < vm.entities.length; i++) {
-                        if (vm.entities[i].hasOwnProperty("speciesName") && x.thing === vm.entities[i].thing) {
+                        if (vm.entities[i].hasOwnProperty("speciesName") && entity.thing === vm.entities[i].thing) {
                             return {
                                 id: vm.entities[i].thing,
                                 name: vm.entities[i].speciesName
@@ -178,21 +190,22 @@
                 }
             });
 
+            // Filter out non duplicates
             alreadyFetchedSpecies = alreadyFetchedSpecies.filter((x) => x !== undefined);
 
             function successCallback(response) {
                 assignFetchedSpeciesNames(response);
                 assignDuplicateSpeciesNames(alreadyFetchedSpecies);
-                vm.fetchedUniqueSpeciesIDs = uniqueSpeciesIDs;
+                vm.fetchedSpeciesIDs = newSpeciesIDs;
             }
 
             function failureCallback(error) {
                 console.error(error);
             }
 
-            if (uniqueSpeciesIDs.length > vm.fetchedUniqueSpeciesIDs.length) {
+            if (newSpeciesIDs.length > vm.fetchedSpeciesIDs.length) {
                 // Among the sightings fetched from the server, at least one of them is of a species that hasn't been requested from the server yet.
-                getUniqueSpecies(uniqueSpeciesIDs.diff(vm.fetchedUniqueSpeciesIDs)).then(successCallback, failureCallback);
+                getNewSpecies(newSpeciesIDs.diff(vm.fetchedSpeciesIDs)).then(successCallback, failureCallback);
             } else {
                 // The new sightings fetched from the server have a thing (species ID) property that has already been fetched which makes them duplicates.
                 assignDuplicateSpeciesNames(alreadyFetchedSpecies)
@@ -200,20 +213,8 @@
 
         }
 
-        function assignDuplicateSpeciesNames(alreadyFetchedSpecies) {
-            for (let i = 0; i < vm.entities.length; i++) {
-                if (!vm.entities[i].hasOwnProperty("speciesName")) {
-                    for (let j = 0; j < alreadyFetchedSpecies.length; j++) {
-                        if (vm.entities[i].thing === alreadyFetchedSpecies[j].id) {
-                            vm.entities[i].speciesName = alreadyFetchedSpecies[j].name;
-                        }
-                    }
-                }
-            }
-        }
-
         /**
-         * Assigns the names of the species for each sighting
+         * Assigns the names of the new species for each sighting that was recieved from the next page request.
          * @param {Object} res Response from the server
          */
         function assignFetchedSpeciesNames(res) {
@@ -228,14 +229,25 @@
             }
         }
 
-        // https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
-        Array.prototype.diff = function (a) {
-            return this.filter(i => a.indexOf(i) < 0)
+        /**
+         * Assigns the names of species that were already requested previously in earlier page requests. This helps avoid unnecessary network requests.
+         * @param {Array} alreadyFetchedSpecies The species that were already fetched for previous page request results
+         */
+        function assignDuplicateSpeciesNames(alreadyFetchedSpecies) {
+            for (let i = 0; i < vm.entities.length; i++) {
+                if (!vm.entities[i].hasOwnProperty("speciesName")) {
+                    for (let j = 0; j < alreadyFetchedSpecies.length; j++) {
+                        if (vm.entities[i].thing === alreadyFetchedSpecies[j].id) {
+                            vm.entities[i].speciesName = alreadyFetchedSpecies[j].name;
+                        }
+                    }
+                }
+            }
         }
 
-        function getUniqueSpecies(uniqueSpeciesIDArr) {
+        function getNewSpecies(newSpeciesIDArr) {
             let promiseObj = $q.defer();
-            let promiseArray = uniqueSpeciesIDArr.map(speciesID => speciesSrvc.getSpeciesFromId(speciesID));
+            let promiseArray = newSpeciesIDArr.map(speciesID => speciesSrvc.getSpeciesFromId(speciesID));
             promiseObj.resolve($q.all(promiseArray));
             promiseObj.reject(new Error("Failed to GET associated unique species for sightings"));
             return promiseObj.promise;
