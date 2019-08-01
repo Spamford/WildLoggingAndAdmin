Spamford/WildLoggingAndAdmin

commit bbc48bf41e5498f1291748651c2405bf9bbf4563

Added search-bar directive
Search bar directive can be passed a callback function that is executed when the search button is pressed.

Need to implement the callback function making changes to the api service to accomodate pagination.

scripts/states/admin/admin_search/adminSearch.directive.js
@@ -1,0 +1,30 @@
+(function () {
+
+    'use strict';
+
+    var app = angular.module('app.adminState');
+
+    app.directive('searchBar', function () {
+        return {
+            restrict: "E",
+            scope: {
+                search: "=",
+                placeholder: "@"
+            },
+            templateUrl: "scripts/states/admin/admin_search/admin.search.template.html",
+            controller: searchCtrl,
+            controllerAs: "vm",
+            bindToController: true
+        }
+    });
+
+    app.controller('searchCtrl', searchCtrl);
+
+    searchCtrl.$inject = [];
+
+    function searchCtrl() {
+        var vm = angular.extend(this, {});
+        return vm;
+    }
+
+})();

index.html
@@ -73,13 +73,14 @@
     <script src="scripts/states/admin/admin.module.js"></script>
     <script src="scripts/states/admin/admin.controller.js"></script>
     <script src="scripts/states/admin/admin_delete/admin.delete.controller.js"></script>
+    <script src="scripts/states/admin/admin_search/adminSearch.directive.js"></script>
 
     <!-- angular-ui-notification -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-notification/0.3.6/angular-ui-notification.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-notification/0.3.6/angular-ui-notification.css"/>
 
   </head>
-  <body ng-app="starter">
+  <body ng-app="starter" ng-cloak>
     <connectionerror show="vm.errorModalShow">
       modal connection error window
       {{vm.errorModalText}}

scripts/states/admin/admin_delete/admin.delete.controller.js
@@ -96,6 +96,10 @@
       return promiseObj.promise;
     }
 
+    vm.getEntities = function getEntities(entityName) {
+      console.log(entityName);
+    }
+
     return vm;
 
   }

scripts/states/admin/admin_delete/admin.delete.html
@@ -1,7 +1,7 @@
 <div class="admin-delete">
     <h3 style="text-align: center;">Delete {{vm.title}}</h3>
     <div class="container-fluid">
-        <input ng-model="vm.searchStr" type="text" class="form-control" placeholder="Name of {{vm.title}}" aria-label="Search">
+        <search-bar search="vm.getEntities" placeholder="Name of {{vm.title}}"></search-bar>
         <div class="form-check">
             <input id="admin-check-all" class="form-check-input" type="checkbox" ng-model="vm.allSelected" ng-change="vm.selectAll()" />
             <label for="admin-check-all" class="form-check-label">Select All</label>

scripts/states/admin/admin_search/admin.search.template.html
@@ -1,0 +1,8 @@
+<!-- https://getbootstrap.com/docs/4.0/utilities/spacing/ -->
+<!-- mt = margin top,  - = syntax,  3 = middle value in range of sizes which equals the value of $spacer. -->
+<div class="input-group mt-3">
+    <input ng-model="vm.searchStr" type="text" class="form-control" placeholder="{{vm.placeholder}}" aria-label="{vm.placeholder}}" aria-describedby="admin-search-button">
+    <div class="input-group-append">
+        <button ng-click="vm.search(vm.searchStr)" class="btn btn-outline-secondary" type="button" id="admin-search-button">Search</button>
+    </div>
+</div>

commit dd2db9f1760920b3f33fec0487460b8f833ccd19

If applied, this commit will let map display properly.
Adding search bar in bbc48bf41e5498f1291748651c2405bf9bbf4563 broke the search module as the controller for the states in the search module had the same name as the controller in the search-bar directive.

Since the search-bar directive was loaded after the search module's files, the controller from the search module was overwritten.

Changing the search-bar directive's controller's name fixed this problem.

scripts/states/admin/admin_search/adminSearch.directive.js
@@ -13,17 +13,17 @@
                 searching: "="
             },
             templateUrl: "scripts/states/admin/admin_search/admin.search.template.html",
-            controller: searchCtrl,
+            controller: searchBarCtrl,
             controllerAs: "vm",
             bindToController: true
         }
     });
 
-    app.controller('searchCtrl', searchCtrl);
+    app.controller('searchBarCtrl', searchBarCtrl);
 
-    searchCtrl.$inject = [];
+    searchBarCtrl.$inject = [];
 
-    function searchCtrl() {
+    function searchBarCtrl() {
         var vm = angular.extend(this, {});
 
         vm.handleEvent = function handleEvent(evt) {

scripts/states/search/search.module.js
@@ -19,7 +19,7 @@
             location: ['$stateParams','locationsSrvc', function( $stateParams, locationsSrvc )  {
               return locationsSrvc.getLocation( $stateParams.postcode ).then(
                 function gotOkay( location ) {
-                  return location.result;
+                  return location.data.result;
                 },
                 function fail( err ) {
                   alert(err);