38215e0a8018b6bda2159f7c1fb88fec6812b708
Athul Philip <athulphilip1309@gmail.com>
2019-08-07 12:06:46 +0100

    Updates client variables and README
    Client variables are now what they are supposed to be so that the authentication can work properly when deployed.
    
    Changes README title to be the same as the name of the repo.

--- client_variables.js
+++ client_variables.js
@@ -2,19 +2,18 @@ const CLIENT_CONFIG = {
   "AUTH0_CLIENT_ID":"Wjp8IumneHRsi22lz2g7mqpjgqgiTfwh",
   "AUTH0_DOMAIN":"myurbanwild.eu.auth0.com",
 
-  // "AUTH0_CALLBACK_URL":"https://cmdt.github.io/ubanwild_wild_logging_auth0_static/admin",
-  "AUTH0_CALLBACK_URL":"http://localhost:5500/#/admin",
+  "AUTH0_CALLBACK_URL":"https://cmdt.github.io/WildLoggingAndAdmin/admin",
+  // "AUTH0_CALLBACK_URL":"http://localhost:5500/#/admin",
 
-  // "APP_SCHEME": "https", 
-  "APP_SCHEME": "http", 
+  "APP_SCHEME": "https", 
+  // "APP_SCHEME": "http", 
 
-  // "APP_DOMAIN": "cmdt.github.io", 
-  "APP_DOMAIN": "localhost", 
+  "APP_DOMAIN": "cmdt.github.io", 
+  // "APP_DOMAIN": "localhost", 
 
-  // "APP_PORT": "443", 
-  "APP_PORT": "5501",
+  "APP_PORT": "443", 
+  // "APP_PORT": "5501",
 
   "AUTH0_AUDIENCE": "https://www.myurbanwild/wildloggingadmin",
-  // "AUTH0_REQUESTED_SCOPES": "openid profile" 
   "AUTH0_REQUESTED_SCOPES": "admin" 
 };
